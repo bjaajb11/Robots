63fe8c06757010574585df23297400c01366b419
Bart Anderson <bjaajb11@gmail.com>
2018-12-10 23:10:56 -0600

    Added first move test (speed 0 does not move)

---
+++ Robots/Assets/Project/Scripts/Mover.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public Vector3 Move(int horizontal, int vertical, int deltaTime)
+    {
+        return Vector3.zero;
+    }
+}
+
---
+++ Robots/Assets/Project/Tests/MoverSpec.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class MoverSpec
+{
+    public class MoveTests
+    {
+
+        [Test]
+        public void SpeedIsZero_DoesNotMove()
+        {
+            var mover = new GameObject().AddComponent<Mover>();
+            Vector3 position = mover.Move(0, 0, 0);
+            Assert.That(position, Is.EqualTo(Vector3.zero));
+        }
+
+
+    }
+}
+
