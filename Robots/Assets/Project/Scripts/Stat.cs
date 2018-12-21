a695a6f30103277e1dede7ee31e9e1a98ba1bfff
Bart Anderson <bjaajb11@gmail.com>
2018-12-20 22:34:00 -0600

    Added stat class

---
+++ Robots/Assets/Project/Scripts/Stat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+
+namespace Project.Scripts
+{
+    [Serializable]
+    public class Stat
+    {
+        [SerializeField] private int _value;
+        public int Value => _value;
+
+        public void SetBaseValue(int value)
+        {
+            _value = value;
+        }
+    }
+}
---
+++ Robots/Assets/Project/Tests/StatsSpec.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using Project.Scripts;
+
+public class StatsSpec
+{
+    public class GetValueTests
+    {
+        [Test]
+        public void NoModifiers_ReturnsBaseValue([Values(100,32)] int value)
+        {
+            var stat = new Stat();
+            stat.SetBaseValue(value);
+            Assert.That(stat.Value, Is.EqualTo(value));
+        }
+    }
+}
