6f3a96ed2585b39f6421452d9d940629571bd478
Bart Anderson <bjaajb11@gmail.com>
2018-12-22 14:02:43 -0600

    Added GameManager actions to show Game over on player death

---
+++ Robots/Assets/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using Project.Scripts.Character;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject _gameOverUI;
+
+    private void Start()
+    {
+        var stats = PlayerManager.Instance.Player.GetComponent<CharacterStats>();
+        stats.DieAction += PlayerDied;
+    }
+
+    private void PlayerDied()
+    {
+        _gameOverUI.SetActive(true);
+    }
+}
